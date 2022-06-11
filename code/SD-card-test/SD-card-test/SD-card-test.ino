0e402e0c7a175ee28f7a754b48b60b2f7e59b898
Peter Harrison <peter.harrison@helicron.net>
2022-06-11 15:20:29 +0100

    reorganise SD card test

--- code/SD-card-test/SD-card-test/SD-card-test.ino
+++ code/SD-card-test/SD-card-test/SD-card-test.ino
@@ -13,15 +13,12 @@
  *
  */
 #include <Arduino.h>
-#include <SD.h>
+
 #include <SPI.h>
 #include <SoftwareSerial.h>
+#include "SdFat.h"
+#include "sdios.h"
 /// Pin Assignments
-const int LED_STATUS = 2;
-const int LED_2 = 2;
-const int LED_3 = 3;
-const int LED_4 = 4;
-const int DISP_SELECT = 5;
 
 const int RADIO_RX = 7;
 const int RADIO_TX = 8;
@@ -51,281 +48,246 @@ const uint32_t ONE_SECOND = 1000L;
 const uint32_t ONE_MINUTE = 60 * ONE_SECOND;
 const uint32_t ONE_HOUR = 60 * ONE_MINUTE;
 
-uint32_t displayUpdateInterval = 1000;
+uint32_t displayUpdateInterval = 100;
 uint32_t displayUpdateTime;
 int state = 0;
 uint32_t animUpdateTime = 0;
 int frameCounter;
 uint32_t systime;
 uint32_t startTime;
-
+// serial output steam
+ArduinoOutStream cout(Serial);
 /***************************************************   SD Card */
 
-const int chipSelect = 10;
-const int cardDetect = 9;
+/*
+ * Set DISABLE_CHIP_SELECT to disable a second SPI device.
+ * For example, with the Ethernet shield, set DISABLE_CHIP_SELECT
+ * to 10 to disable the Ethernet controller.
+ */
+const int8_t DISABLE_CHIP_SELECT = -1;
+// store error strings in flash to save RAM
+#define error(s) sd.errorHalt(F(s))
+SdFat sd;
+#define FILE_BASE_NAME "Data"
+const uint8_t BASE_NAME_SIZE = sizeof(FILE_BASE_NAME) - 1;
+char fileName[13] = FILE_BASE_NAME "00.csv";
+uint32_t logTime;
+int logLines = 0;
+// Log file.
+SdFile logFile;
+const uint8_t ANALOG_COUNT = 4;
 
-void sdCardInit() {
-  pinMode(chipSelect, OUTPUT);
-  pinMode(cardDetect, INPUT_PULLUP);
-  // we'll use the initialization code from the utility libraries
-  // since we're just testing if the card is working!
- if (!SD.begin(chipSelect)) {
-    Serial.println("initialization failed!");
-    return;
+//------------------------------------------------------------------------------
+/*
+ * date/time values for debug
+ * normally supplied by a real-time clock or GPS
+ */
+// date 1-Oct-14
+uint16_t year = 2014;
+uint8_t month = 10;
+uint8_t day = 1;
+
+// time 20:30:40
+uint8_t hour = 20;
+uint8_t minute = 30;
+uint8_t second = 40;
+//------------------------------------------------------------------------------
+/*
+ * User provided date time callback function.
+ * See SdFile::dateTimeCallback() for usage.
+ */
+void dateTime(uint16_t* date, uint16_t* time) {
+  // User gets date and time from GPS or real-time
+  // clock in real callback function
+
+  // return date using FAT_DATE macro to format fields
+  *date = FAT_DATE(year, month, day);
+
+  // return time using FAT_TIME macro to format fields
+  *time = FAT_TIME(hour, minute, second);
+}
+//------------------------------------------------------------------------------
+/*
+ * Function to print all timestamps.
+ */
+void printTimestamps(SdFile& f) {
+  dir_t d;
+  if (!f.dirEntry(&d)) {
+    error("f.dirEntry failed");
   }
-  Serial.println(F("Card present"));
+
+  cout << F("Creation: ");
+  f.printFatDate(d.creationDate);
+  cout << ' ';
+  f.printFatTime(d.creationTime);
+  cout << endl;
+
+  cout << F("Modify: ");
+  f.printFatDate(d.lastWriteDate);
+  cout << ' ';
+  f.printFatTime(d.lastWriteTime);
+  cout << endl;
+
+  cout << F("Access: ");
+  f.printFatDate(d.lastAccessDate);
+  cout << ' ';
+  f.printFatTime(d.lastWriteTime);
+  cout << endl;
+}
+//------------------------------------------------------------------------------
+void dateTest() {
+  SdFile file;
+  // remove files if they exist
+  cout << F("Removing old files\n");
+  sd.remove("callback.txt");
+  sd.remove("default.txt");
+  sd.remove("stamp.txt");
+  // create a new file with default timestamps
+  if (!file.open("default.txt", O_WRONLY | O_CREAT)) {
+    error("open default.txt failed");
+  }
+  cout << F("\nOpen with default times\n");
+  printTimestamps(file);
+
+  // close file
+  file.close();
+  /*
+   * Test the date time callback function.
+   *
+   * dateTimeCallback() sets the function
+   * that is called when a file is created
+   * or when a file's directory entry is
+   * modified by sync().
+   *
+   * The callback can be disabled by the call
+   * SdFile::dateTimeCallbackCancel()
+   */
+  // set date time callback function
+  SdFile::dateTimeCallback(dateTime);
+
+  // create a new file with callback timestamps
+  if (!file.open("callback.txt", O_WRONLY | O_CREAT)) {
+    error("open callback.txt failed");
+  }
+  cout << ("\nOpen with callback times\n");
+  printTimestamps(file);
+
+  // change call back date
+  year += 1;
+  day += 1;
+  // must add two to see change since FAT second field is 5-bits
+  second += 2;
+
+  // modify file by writing a byte
+  file.write('t');
+
+  // force dir update
+  file.sync();
+
+  cout << F("\nTimes after write\n");
+  printTimestamps(file);
+
+  // close file
+  file.close();
+  /*
+   * Test timestamp() function
+   *
+   * Cancel callback so sync will not
+   * change access/modify timestamp
+   */
+  SdFile::dateTimeCallbackCancel();
+
+  // create a new file with default timestamps
+  if (!file.open("stamp.txt", O_WRONLY | O_CREAT)) {
+    error("open stamp.txt failed");
+  }
+  // set creation date time
+  if (!file.timestamp(T_CREATE, 2014, 11, 10, 1, 2, 3)) {
+    error("set create time failed");
+  }
+  // set write/modification date time
+  if (!file.timestamp(T_WRITE, 2014, 11, 11, 4, 5, 6)) {
+    error("set write time failed");
+  }
+  // set access date
+  if (!file.timestamp(T_ACCESS, 2014, 11, 12, 7, 8, 9)) {
+    error("set access time failed");
+  }
+  cout << F("\nTimes after timestamp() calls\n");
+  printTimestamps(file);
+
+  file.close();
+  cout << F("\ndateTest() done\n");
+  return;
+}
+//------------------------------------------------------------------------------
+
+// global for card size
+uint32_t cardSize;
+
+// global for card erase size
+uint32_t eraseSize;
+//------------------------------------------------------------------------------
+// store error strings in flash
+#define sdErrorMsg(msg) sd.errorPrint(F(msg));
+//------------------------------------------------------------------------------
+uint8_t cidDump() {
+  cid_t cid;
+  if (!sd.card()->readCID(&cid)) {
+    sdErrorMsg("readCID failed");
+    return false;
+  }
+  cout << F("\nManufacturer ID: ");
+  cout << hex << int(cid.mid) << dec << endl;
+  cout << F("OEM ID: ") << cid.oid[0] << cid.oid[1] << endl;
+  cout << F("Product: ");
+  for (uint8_t i = 0; i < 5; i++) {
+    cout << cid.pnm[i];
+  }
+  cout << F("\nVersion: ");
+  cout << int(cid.prv_n) << '.' << int(cid.prv_m) << endl;
+  cout << F("Serial number: ") << hex << cid.psn << dec << endl;
+  cout << F("Manufacturing date: ");
+  cout << int(cid.mdt_month) << '/';
+  cout << (2000 + cid.mdt_year_low + 10 * cid.mdt_year_high) << endl;
+  cout << endl;
+  return true;
 }
 
-void cardInfo() {
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  File myFile = SD.open("TEST.TXT", FILE_WRITE);
-
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print(F("Writing to test.txt..."));
-    myFile.println(F("testing 1, 2, 3, 4."));
-    // close the file:
-    myFile.close();
-    Serial.println(F("done."));
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println(F("error opening test.txt"));
-  }
-
-  // re-open the file for reading:
-  myFile = SD.open("TEST.TXT");
-  if (myFile) {
-    Serial.println("test.txt:");
-
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial.write(myFile.read());
-    }
-    // close the file:
-    myFile.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
-
-  // Serial.println(F("\nFiles found on the card (name, date and size in bytes): "));
-  // root.openRoot(volume);
-  myFile = SD.open("/");
-  printDirectory(myFile, 0);
-  myFile.close();
-}
-
-void printDirectory(File dir, int numTabs) {
-  // Begin at the start of the directory
-  dir.rewindDirectory();
-  while (true) {
-    File entry = dir.openNextFile();
-    if (!entry) {
-      // no more files
-      Serial.println(F("**nomorefiles**"));
-      break;
-    }
-    for (uint8_t i = 0; i < numTabs; i++) {
-      Serial.print('\t');  // we'll have a nice indentation
-    }
-    // Print the 8.3 name
-    Serial.print(entry.name());
-    // Recurse for directories, otherwise print the file size
-    if (entry.isDirectory()) {
-      Serial.println('/');
-      printDirectory(entry, numTabs + 1);
-    } else {
-      // files have sizes, directories do not
-      Serial.print("\t\t");
-      Serial.println(entry.size(), DEC);
-    }
-    entry.close();
-  }
-}
 /***************************************************   SD Card End */
 
-
-/***************************************************   7-SEGMENT LED START */
-
-#include <SPI.h>
-const static byte charTable[] PROGMEM = {
-    B01111110,  // 0
-    B00110000,  // 1
-    B01101101,  // 2
-    B01111001,  // 3
-    B00110011,  // 4
-    B01011011,  // 5
-    B01011111,  // 6
-    B01110000,  // 7
-    B01111111,  // 8
-    B01111011,  // 9
-
-    B01110111,  // A
-    B00011111,  // B
-    B01001110,  // C
-    B00111101,  // D
-    B01001111,  // E
-    B01000111,  // F
-    B00000001,  // -
-
-    B01111101,  // a
-    B00011111,  // b
-    B00001101,  // c
-    B00111101,  // d
-    B01101111,  // e
-    B01000111,  // F
-    B01011110,  // G
-    B00010111,  // h
-    B00000110,  // l
-    B00111100,  // J
-    B00001110,  // L
-    B00010101,  // n
-    B00011101,  // o
-    B01100111,  // P
-    B01100110,  // r
-    B00001111,  // t
-    B00111110,  // U
-    B00110111,  // X
-    B00111011,  // Y
-
-    B01000000,  // spinner
-    B00100000,  // spinner
-    B00010000,  // spinner
-    B00001000,  // spinner
-    B00000100,  // spinner
-    B00000010,  // spinner
-
-};
-
-
-class LedDisplay {
- private:
-  const int REG_DECODEMODE = 9;
-  const int REG_BRIGHTNESS = 10;
-  const int REG_SCANLIMIT = 11;
-  const int REG_SHUTDOWN = 12;
-  const int REG_DISPLAYTEST = 15;
-  uint8_t mCS = NOT_A_PIN;
-  uint8_t mLastDigit = 7;
-  void writeRegister(uint8_t reg, uint8_t val) {
-    if (!mCS) {
-      return;
-    }
-    digitalWrite(mCS, LOW);  // digit 0
-    SPI.transfer(reg);
-    SPI.transfer(val);
-    digitalWrite(mCS, HIGH);
-  }
-
- public:
-  LedDisplay(int cs_pin = NOT_A_PIN) : mCS(cs_pin) {}
-
-  void begin() {
-    SPI.setClockDivider(SPI_CLOCK_DIV2);
-    SPI.setDataMode(SPI_MODE0);
-    SPI.setBitOrder(MSBFIRST);
-    digitalWrite(mCS, HIGH);
-    pinMode(mCS, OUTPUT);
-    SPI.begin();
-    wakeUp();
-    setDecode(0x00);
-    setDigitLimit(7);
-    for (int i = 1; i < 9; i++) {  // set all digits OFF
-      writeRegister(i, 0x0A);
-    }
-  }
-  void setBrightness(uint8_t bright) { writeRegister(REG_BRIGHTNESS, bright & 0x0F); }
-  void setDigitLimit(uint8_t limit) {
-    mLastDigit = limit;
-    writeRegister(REG_SCANLIMIT, mLastDigit);
-  }
-  void wakeUp() { writeRegister(REG_SHUTDOWN, 1); }
-  void shutDown() { writeRegister(REG_SHUTDOWN, 0); }
-  void setDecode(uint8_t digits) { writeRegister(REG_DECODEMODE, digits); };
-  void setDisplayTest(bool state) { writeRegister(REG_DISPLAYTEST, state ? 1 : 0); }
-  void writeDigit(uint8_t digit, uint8_t value) {
-    byte tableValue;
-    tableValue = pgm_read_byte_near(charTable + value);
-    writeRegister(digit + 1, tableValue);
-  }
-
-  void writeDigits(uint8_t digits[]) {
-    for (int i = 0; i < 8; i++) {
-      uint8_t data = pgm_read_byte_near(charTable + digits[i]);
-      if ((i == 3) || (i == 5)) {
-        data |= 0x80;  // DP on
-      }
-      writeRegister(i + 1, data);
-    }
-  }
-
-  void clear() {
-    for (int i = 1; i < 9; i++) {
-      byte tableValue;
-      tableValue = pgm_read_byte_near(charTable + 16);
-      writeRegister(i, tableValue);
-    }
-  }
-};
-#define CSpin 5
-LedDisplay ledDisplay(5);
-uint8_t xxx = 0;
-uint8_t yyy = 50;
-
-void displayCounter() {
-  byte digits[8];
-  uint32_t elapsed = millis() - startTime;
-  uint32_t seconds = (elapsed / ONE_SECOND) % 60;
-  uint32_t minutes = (elapsed / ONE_MINUTE);
-  elapsed %= 1000;
-  for (int i = 0; i < 3; i++) {
-    digits[i] = elapsed % 10;
-    elapsed /= 10;
-  }
-  digits[3] = seconds % 10;
-  digits[4] = (seconds / 10) % 10;
-  digits[5] = minutes % 10;
-  digits[6] = 0x10;
-  xxx++;
-  if (xxx >= sizeof(charTable)) {
-    xxx = 0;
-  }
-  digits[7] = xxx;
-  ledDisplay.writeDigits(digits);
-}
-
-void setupLED() {
-  ledDisplay.begin();
-  ledDisplay.setBrightness(1);
-}
-
-/*************************************************************** LED END*/
-
-
-
 /******************************************************** SETUP *****/
+int freeRam() {
+  extern int __heap_start, *__brkval;
+  int v;
+  return (int)&v - (__brkval == 0 ? (int)&__heap_start : (int)__brkval);
+}
 void setup() {
-  pinMode(LED_STATUS, OUTPUT);
-  pinMode(LED_3, OUTPUT);
-  pinMode(LED_4, OUTPUT);
-  pinMode(DISP_SELECT, OUTPUT);
-
+  pinMode(SD_SELECT, OUTPUT);
   pinMode(SD_DETECT, INPUT_PULLUP);
   pinMode(BUTTON_RESET_BACK, INPUT_PULLUP);
   pinMode(BUTTON_TOUCH_OK, INPUT_PULLUP);
   pinMode(BUTTON_GOAL_DN, INPUT_PULLUP);
   pinMode(BUTTON_START_UP, INPUT_PULLUP);
 
-  Serial.begin(9600);
+  Serial.begin(57600);
   while (!Serial) {
-    ;  // Needed for native USB port only
+    SysCall::yield();
   }
-  // sdCardInit();
-  // cardInfo();
-  setupLED();
 
+  // use uppercase in hex and use 0X base prefix
+  // cout << ios::uppercase << showbase << endl;
+  // cout. flags(ios::uppercase);
+  cout << uppercase << showbase << endl;
+
+  // F stores strings in flash to save RAM
+  cout << F("SdFat version: ") << SD_FAT_VERSION << endl;
+  sdInit();
+  dateTest();
+  SdFile::dateTimeCallback(dateTime);
   radio.begin(10000);
+  Serial.print(F("-SRAM left="));
+  Serial.println(freeRam());
 }
 
 /*********************************************** process radio data ***/
@@ -339,8 +301,150 @@ char getRadioChar() {
   return radio.read();
 }
 
+//------------------------------------------------------------------------------
+// Write data header.
+void writeHeader(SdFile & file) {
+  file.print(F("micros"));
+  for (uint8_t i = 0; i < ANALOG_COUNT; i++) {
+    file.print(F(",adc"));
+    file.print(i, DEC);
+  }
+  file.println();
+}
+//------------------------------------------------------------------------------
+// Log a data record.
+void logData(SdFile & file) {
+  uint16_t data[ANALOG_COUNT];
+
+  // Read all channels to avoid SD write latency between readings.
+  for (uint8_t i = 0; i < ANALOG_COUNT; i++) {
+    data[i] = analogRead(i);
+  }
+  // Write data to file.  Start with log time in micros.
+  file.print(logTime);
+
+  // Write ADC data to CSV record.
+  for (uint8_t i = 0; i < ANALOG_COUNT; i++) {
+    file.write(',');
+    file.print(data[i]);
+  }
+  file.println();
+}
+
+void createFile() {
+  // Find an unused file name.
+  if (BASE_NAME_SIZE > 6) {
+    error("FILE_BASE_NAME too long");
+  }
+  while (sd.exists(fileName)) {
+    if (fileName[BASE_NAME_SIZE + 1] != '9') {
+      fileName[BASE_NAME_SIZE + 1]++;
+    } else if (fileName[BASE_NAME_SIZE] != '9') {
+      fileName[BASE_NAME_SIZE + 1] = '0';
+      fileName[BASE_NAME_SIZE]++;
+    } else {
+      error("Can't create file name");
+    }
+  }
+  if (!logFile.open(fileName, O_WRONLY | O_CREAT | O_EXCL)) {
+    error("file.open");
+  }
+  // Read any Serial data.
+  do {
+    delay(10);
+  } while (Serial.available() && Serial.read() >= 0);
+
+  Serial.print(F("Logging to: "));
+  Serial.println(fileName);
+  Serial.println(F("Type any character to stop"));
+}
+
+void rootDir() {
+  SdFile root;
+  SdFile file;
+  // SdFile file;
+  if (!root.open("/")) {
+    sd.errorHalt("open root failed");
+  }
+  int fileCount = 0;
+  // Open next file in root.
+  // Warning, openNext starts at the current directory position
+  // so a rewind of the directory may be required.
+  while (file.openNext(&root, O_RDONLY)) {
+    fileCount++;
+    file.printFileSize(&Serial);
+    Serial.write(' ');
+    file.printModifyDateTime(&Serial);
+    Serial.write(' ');
+    file.printName(&Serial);
+    if (file.isDir()) {
+      // Indicate a directory.
+      Serial.write('/');
+    }
+    Serial.println();
+    file.close();
+  }
+  if (root.getError()) {
+    Serial.println("openNext failed");
+  }
+  cout << F("\nTotal ") << fileCount << endl;
+  root.close();
+}
+/*********************************************** SD CARD ******************/
+void sdInit() {
+  uint32_t t = millis();
+  Serial.print(F("Card Initialising ..."));
+  // Initialize at the highest speed supported by the board that is
+  // not over 50 MHz. Try a lower speed if SPI errors occur.
+  if (!sd.cardBegin(SD_SELECT, SD_SCK_MHZ(50))) {
+    sdErrorMsg("cardBegin failed");
+    return;
+  }
+  t = millis() - t;
+  cardSize = sd.card()->cardSize();
+  if (cardSize == 0) {
+    sdErrorMsg("cardSize failed");
+    return;
+  }
+  // cout << F("\ninit time: ") << t << " ms" << endl;
+  cout << F(" Card type: ");
+  switch (sd.card()->type()) {
+    case SD_CARD_TYPE_SD1:
+      cout << F("SD1");
+      break;
+
+    case SD_CARD_TYPE_SD2:
+      cout << F("SD2");
+      break;
+
+    case SD_CARD_TYPE_SDHC:
+      if (cardSize < 70000000) {
+        cout << F("SDHC");
+      } else {
+        cout << F("SDXC");
+      }
+      break;
+
+    default:
+      cout << F("Unknown");
+  }
+  Serial.print(F(" ... "));
+
+  if (!sd.fsBegin()) {
+    sdErrorMsg("\nFile System initialization failed.\n");
+    return;
+  }
+  Serial.println(F("File system initialised"));
+  // cidDump();
+  sd.ls(LS_R | LS_DATE | LS_SIZE);
+  createFile();
+  rootDir();
+}
+
 /*********************************************** main loop ******************/
 void loop() {  // run over and over
+  static bool logging = true;
+
   if (radio.available()) {
     if (radio.read() == '*') {
     }
@@ -351,16 +455,21 @@ void loop() {  // run over and over
   if (millis() > displayUpdateTime) {
     displayUpdateTime += displayUpdateInterval;
     state = 1 - state;
+    if (!logging) {
+      return;
+    }
+    logLines++;
+    cout << logLines << ',';
+    if (logLines > 10) {
+      logFile.close();
+      Serial.println(F("Finished"));
+      logging = false;
+    } else {
+      logData(logFile);
+      // Force data to SD and update the directory entry to avoid data loss.
+      if (!logFile.sync() || logFile.getWriteError()) {
+        error("write error");
+      }
+    }
   }
-
-  if (millis() > animUpdateTime) {
-    animUpdateTime += 200;
-    digitalWrite(DISP_SELECT, LOW);  // digit 0
-    SPI.transfer(1);
-    SPI.transfer(millis()%10);
-    digitalWrite(DISP_SELECT, HIGH);
-  }
-  digitalWrite(LED_4, digitalRead(BUTTON_TOUCH_OK));
-  digitalWrite(LED_3, digitalRead(BUTTON_GOAL_DN));
-  digitalWrite(LED_2, digitalRead(BUTTON_START_UP));
 }
