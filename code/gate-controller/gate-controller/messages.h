924fc2c632c0bbe6b843ac49aa71aaace34cfd64
Peter Harrison <peter.harrison@helicron.net>
2022-06-11 15:31:24 +0100

    update description of messages used

--- code/gate-controller/gate-controller/messages.h
+++ code/gate-controller/gate-controller/messages.h
@@ -1,50 +1,50 @@
 #include <Arduino.h>
 
 // clang-format off
-// clang-format does not know about .ino files?
 /***
- Author: David Hannaford & Ian Butterworth  Date: 25 September 2017  Version: 1.1
+Author: David Hannaford & Ian Butterworth  Date: 25 September 2017  Version: 6
+Modified: Peter Harrison                   Date:  7 June      2022  Version: 7.0
+Arduino and Visual Basic: Test code for mouse timing system
+Timing performed by Arduino and data passed to VisualBasic PC software
+Overall supervision performed by VisualBasic and passed to the Arduino
+Messages in the format <message_type,value>CrLf
+Implemented message types:
+   ENCODED  MESSAGE TYPE      DIRECTION      TX FREQENCY     COMMENTS
+   98       MSG_NewMouse      PC to Arduino  Event Driven    A new mouse has been selected in the host application 
+                                                             (value argument will always be passed as 0)
+   4        MSG_CURRENT_STATE Arduino to PC                  Value of timer state (0 to 5) 
+                                                                  0 calibrate gates, 
+                                                                  1 looking for mouse in start cell,
+                                                                  2 Mouse seen in start cell, 
+                                                                  3 Run started (but not cleared start gate yet)
+                                                                  4 Run in progress, 
+                                                                  5 Run to centre completed (finish gete triggered)
+                                                                  6 New Mouse
+ 
+   12       MSG_C1SplitTime   Arduino to PC  Event Driven    Time in milliseconds for the current mouse on its current run 
+                                                             (only sent as zero to start host counter)
+   13       MSG_C1RunTime     Arduino to PC  Event Driven    Time in milliseconds for a run that has just completed 
+                                                             (definitive time used to calculate score time - sent twice)
+   30       MSG_CourseTimeMs  Arduino to PC  Event Driven    Time in milliseconds that the current mouse has been active 
+                                                             in the maze (only sent as zero to reset host counter)
 
- Arduino and Visual Basic: Test code for mouse timing system
- Timing performed by Arduino and data passed to VisualBasic PC software
- Overall supervision performed by VisualBasic and passed to the Arduino
+   71       MSG_STrigger      Arduino to PC  Event Driven    New value of Start Gate trigger (Valid values: 1, 0)
+   72       MSG_FTrigger      Arduino to PC  Event Driven    New value of Finish Gate trigger (Valid values: 1, 0)
+   73       MSG_CTrigger      Arduino to PC  Event Driven    New value of Mouse in Start Cell trigger (Valid values: 1,0)
 
- Messages in the format <message_type,value>CrLf
- Supported message types:
-    VAL  TYPE        DIR  TX FREQENCY   COMMENTS
-    1    MazeTime    OUT  100 msec      time in 100 millisecond units that the current mouse has been
-                                        active in the maze
-    2    SplitTime   OUT  10 msec       time in 10 millisecond units for the current mouse on its
-                                        current run (only used to display current run time)
-    3    RunTime     OUT  Event         time in 10 millisecond units for a run that has just completed
-                                        (definitive time used to calculate score time)
-    4    Stateval    OUT                Value of timer state (0 to 5)
-                                              0 calibrate gates,
-                                              1 looking for mouse in start cell,
-                                              2 Mouse seen in start cell,
-                                              3 Run started (but not cleared start gate yet)
-                                              4 Run in progress,
-                                              5 Run to centre completed (finish gete triggered)
-    5    RunTimeMs   OUT  Event         time in 1 millisecond units for a run that has just completed
-                                        (definitive time used to calculate score time)
-    6    SplitToRun  OUT  Event         Asks the supervisor to use the current split time as a run time
-                                        - supervisor timing (value argument will always be passed as 0)
-    71   STrigger    OUT  Event         New value of Start Gate trigger (Valid values: ON, OFF)
-    72   FTrigger    OUT  Event         New value of Finish Gate trigger (Valid values: ON, OFF)
-    73   CTrigger    OUT  Event         New value of Mouse in Start Cell trigger (Valid values: ON, OFF
-    81   SGLevel     OUT  100 msec      Intensity level being received by Start Gate phototransistor
-    82   SGPot       OUT  100 msec      Value read from Start Gate potentiometer
-    83   FGLevel     OUT  100 msec      Intensity level being received by Finish Gate phototransistor
-    84   FGPot       OUT  100 msec      Value read from Finish Gate potentiometer
-    85   SCLevel     OUT  100 msec      Intensity level being received by Mouse in Start Cell phototransistor
-    86   SCPot       OUT  100 msec      Value read from Mouse in Start Cell potentiometer
-    98   NewMouse     IN  Event         A new mouse has been selected in the windows supervision application
-                                        (value argument will always be passed as 0)
-    99   SetMode      IN  Event         Controls the Arduino mode
-                                        Valid values:
-                                              TIMER (normal timing mode),
-                                              CALIBRATION (start returning calibration data)
-*/
+   81       MSG_SGLevel       Arduino to PC  100 msec        Intensity level being received by Start Gate phototransistor
+   82       MSG_SGPot         Arduino to PC  100 msec        Value read from Start Gate potentiometer
+   83       MSG_FGLevel       Arduino to PC  100 msec        Intensity level being received by Finish Gate phototransistor
+   84       MSG_FGPot         Arduino to PC  100 msec        Value read from Finish Gate potentiometer
+   85       MSG_SCLevel       Arduino to PC  100 msec        Intensity level being received by Mouse in Start Cell phototransistor
+   86       MSG_SCPot         Arduino to PC  100 msec        Value read from Mouse in Start Cell potentiometer
+
+   99       MSG_SetMode       PC to Arduino  Event Driven    Controls the Arduino mode 
+                                                             Valid values: 
+                                                                  TIMER       (normal timing mode), 
+                                                                  CALIBRATION (start returning calibration data)
+
+***/
 // clang-format on
 
 #define MSG_MAZE_TIME 1
